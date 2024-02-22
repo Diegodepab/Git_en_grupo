84505969258f7faa0561670fcbeab3b71842fd03
Diegodepab <depablodiegouma@gmail.com>
2024-02-22 18:31:41 +0100

    Se creo las propiedades de capacidad y cantidad

--- Jarra.java
+++ Jarra.java
@@ -4,15 +4,22 @@ public class Jarra { //atributos
 //constructor
     public Jarra(int capacidadInicial){
         if(capacidadInicial<=0){
-            throw new RuntimeException("ERROR: no se pueden crear jarras
-            con capacidad menor o igual que cero.");
+            throw new RuntimeException("ERROR: no se pueden crear jarras con capacidad menor o igual que cero.");
         }
         this.capacidad=capacidadInicial;
     }
     // metodos
-    // 1
-    // 2
-    // 3
+    // 1 CapacidadCantidad_programador1
+	public int capacidad(){
+        return this.capacidad;
+    }
+    public int cantidad(){
+        return this.cantidad;
+    }
+	
+    // 2 llenaVacia_programador2
+	
+    // 3 llenadesdeTostring_programador2
     public void metodoComunitario(){
         // Comentario incluido para pruebas
         System.out.println("Aqui escribimos todos");
