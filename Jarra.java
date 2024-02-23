72b16d2c9c0e6a3e3a21ca6ddd2acaaac7d53689
JuanSoM <0610948742@uma.es>
2024-02-23 09:18:54 +0100

    metodo 2

--- Jarra.java
+++ Jarra.java
@@ -4,14 +4,19 @@ public class Jarra { //atributos
 //constructor
     public Jarra(int capacidadInicial){
         if(capacidadInicial<=0){
-            throw new RuntimeException("ERROR: no se pueden crear jarras
-            con capacidad menor o igual que cero.");
+            throw new RuntimeException("ERROR: no se pueden crear jarra con capacidad menor o igual que cero.");
         }
         this.capacidad=capacidadInicial;
     }
     // metodos
     // 1
     // 2
+    public void llena() {
+        this.cantidad=this.capacidad;
+    }
+    public void vacia() {
+        this.cantidad=0;
+    }
     // 3
     public void metodoComunitario(){
         // Comentario incluido para pruebas
